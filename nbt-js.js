41ef98f4899a120dbe73ee6d14498f5e5acf0a4f
Jonathan Faulch <jonathanfaulch@gmail.com>
2023-01-14 22:02:13 +0100

    Modernize the code a bit
    * Use const/let
    * Use arrow functions if appropriate
    * Reflect my new preference in the bracket holy war

--- nbt-js.js
+++ nbt-js.js
@@ -2,11 +2,10 @@
  * Types                                                                       *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-var typeFields =
+const typeFields =
 	['name'     , 'structure', 'format'  , 'size'];
-	
-var types = exports.types =
-[
+
+const types = exports.types = [
 	['end'      , null       , null      , null  ],
 	['byte'     , 'word'     , 'Int8'    , 1     ],
 	['short'    , 'word'     , 'Int16BE' , 2     ],
@@ -21,68 +20,55 @@ var types = exports.types =
 	['intArray' , 'list'     , 'int'     , null  ]
 ];
 
-types.forEach(function(typeData, typeIndex)
-{
-	var type = { value: typeIndex };
-	typeFields.forEach(function(propertyName, propertyIndex)
-	{
-		type[propertyName] = typeData[propertyIndex];
-	});
+types.forEach((typeData, typeIndex) => {
+	const type = {value: typeIndex};
+	typeFields.forEach((propertyName, propertyIndex) => type[propertyName] = typeData[propertyIndex]);
 	types[type.value] = types[type.name] = type;
 });
 
-types.fromSchema = function(schema)
-{
-	return typeof schema === 'string' ? types[schema] :
-		(Array.isArray(schema) ? types.list : types.compound);
-};
+types.fromSchema = schema => typeof schema === 'string' ? types[schema] :
+	(Array.isArray(schema) ? types.list : types.compound);
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
  * Reader                                                                      *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-var Reader = exports.Reader = function(buffer)
-{
+const Reader = exports.Reader = function (buffer) {
 	this.buffer = buffer;
 	this.offset = 0;
 };
 
-function read(reader, object)
-{
-	var type = types[reader.byte().payload];
-	if (type !== types.end)
-	{
-		var name = reader.string().payload;
-		var result = reader[type.name]();
+function read(reader, object) {
+	const type = types[reader.byte().payload];
+	if (type !== types.end) {
+		const name = reader.string().payload;
+		const result = reader[type.name]();
 		object.schema[name] = result.schema;
 		object.payload[name] = result.payload;
 	}
 	return type;
 }
 
-types.forEach(function(type)
-{
-	switch(type.structure)
-	{
+types.forEach(type => {
+	switch (type.structure) {
 		case 'word':
-			Reader.prototype[type.name] = function()
-			{
-				var word = this.buffer['read' + type.format](this.offset);
+			Reader.prototype[type.name] = function () {
+				const word = this.buffer['read' + type.format](this.offset);
 				this.offset += type.size;
-				return { schema: type.name, payload: word };
+				return {schema: type.name, payload: word};
 			};
 			break;
 		case 'list':
-			var isList = type === types.list;
-			Reader.prototype[type.name] = function()
-			{
-				var typeName = type.format || types[this.byte().payload].name;
-				var result = { schema: isList ? [ typeName ] : type.name, payload: [] };
-				var length = type.size || this.int().payload;
-				for (var i = 0; i < length; i++)
-				{
-					var element = this[typeName]();
-					if (isList) { result.schema = [ element.schema ]; }
+			const isList = type === types.list;
+			Reader.prototype[type.name] = function () {
+				const typeName = type.format || types[this.byte().payload].name;
+				const result = {schema: isList ? [typeName] : type.name, payload: []};
+				const length = type.size || this.int().payload;
+				for (let i = 0; i < length; i++) {
+					const element = this[typeName]();
+					if (isList) {
+						result.schema = [element.schema];
+					}
 					result.payload.push(element.payload);
 				}
 				return result;
@@ -91,9 +77,8 @@ types.forEach(function(type)
 	}
 });
 
-Reader.prototype[types.string.name] = function()
-{
-	var length = this.short().payload;
+Reader.prototype[types.string.name] = function () {
+	const length = this.short().payload;
 	return new Object
 	({
 		schema: types.string.name,
@@ -101,16 +86,14 @@ Reader.prototype[types.string.name] = function()
 	});
 };
 
-Reader.prototype[types.compound.name] = function()
-{
-	var result = { schema: {}, payload: {} };
-	while (read(this, result) !== types.end);
+Reader.prototype[types.compound.name] = function () {
+	const result = {schema: {}, payload: {}};
+	while (read(this, result) !== types.end) {}
 	return result;
 };
 
-exports.read = function(buffer)
-{
-	var result = { schema: {}, payload: {} };
+exports.read = buffer => {
+	const result = {schema: {}, payload: {}};
 	read(new Reader(buffer), result);
 	return result;
 };
@@ -119,53 +102,44 @@ exports.read = function(buffer)
  * Writer                                                                      *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-var write = exports.write = function()
-{
-	return write[types.compound.name].apply(this, arguments);
-};
+const write = exports.write = (...args) => write[types.compound.name](...args);
 
-types.forEach(function(type)
-{
-	switch(type.structure)
-	{
+types.forEach(type => {
+	switch (type.structure) {
 		case 'word':
-			write[type.name] = function(value)
-			{
-				var buffer = new Buffer(type.size);
+			write[type.name] = value => {
+				const buffer = new Buffer(type.size);
 				buffer['write' + type.format](value);
 				return buffer;
 			};
 			break;
 		case 'list':
-			var isList = type === types.list;
-			write[type.name] = function(value, schema)
-			{
-				var typeName = isList ? types.fromSchema(schema[0]).name : types[schema].format;
-				var buffers = [];
-				if (isList) { buffers.push(write.byte(types[typeName].value)); }
-				if (!type.size) { buffers.push(write.int(value.length)); }
-				value.forEach(function(element)
-				{
-					buffers.push(write[typeName](element, schema[0]));
-				});
+			const isList = type === types.list;
+			write[type.name] = (value, schema) => {
+				const typeName = isList ? types.fromSchema(schema[0]).name : types[schema].format;
+				const buffers = [];
+				if (isList) {
+					buffers.push(write.byte(types[typeName].value));
+				}
+				if (!type.size) {
+					buffers.push(write.int(value.length));
+				}
+				value.forEach(element => buffers.push(write[typeName](element, schema[0])));
 				return Buffer.concat(buffers);
 			};
 			break;
 	}
 });
 
-write[types.string.name] = function(value)
-{
-	var buffer = new Buffer(value, 'utf8');
+write[types.string.name] = value => {
+	const buffer = new Buffer(value, 'utf8');
 	return Buffer.concat([write.short(buffer.length), buffer]);
 };
 
-write[types.compound.name] = function(value, schema)
-{
-	var buffers = [];
-	for (var name in value)
-	{
-		var type = types.fromSchema(schema[name]);
+write[types.compound.name] = (value, schema) => {
+	const buffers = [];
+	for (const name in value) {
+		const type = types.fromSchema(schema[name]);
 		buffers.push(write.byte(type.value));
 		buffers.push(write.string(name));
 		buffers.push(write[type.name](value[name], schema[name]));
